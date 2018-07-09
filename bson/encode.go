04b6cd516663d1f91d8f914e66ae743d3ab1685c
TeoV <teofil.voivozeanu@itsyscom.com>
2018-07-09 04:01:51 -0400

    Encode time as nanoseconds

--- bson/encode.go
+++ bson/encode.go
@@ -559,7 +559,7 @@ func (e *encoder) addElem(name string, v reflect.Value, minSize bool) {
 		case time.Time:
 			// MongoDB handles timestamps as milliseconds.
 			e.addElemName(0x09, name)
-			e.addInt64(s.Unix()*1000 + int64(s.Nanosecond()/1e6))
+			e.addInt64(s.Unix()*1000 + int64(s.Nanosecond()))
 
 		case url.URL:
 			e.addElemName(0x02, name)
